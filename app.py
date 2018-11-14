cfe0c671687c0e633ce4e3889d6cad1772c251b7
denisturygin <chlen@denisa.ru>
2018-11-15 00:49:56 +0700

    .

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file
+from pathlib import Path
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/list')
+def list():
+    directory = Path(request.args['directory'])
+    assert (directory.is_dir())
+    result = []
+    for i in directory.iterdir():
+        result.append({
+            "name": i.name,
+            "directory": i.is_dir()
+        })
+    return jsonify(result)
+
+
+@app.route('/get')
+def get():
+    file = Path(request.args['file'])
+    return send_file(str(file), attachment_filename=file.name)
 
 
 if __name__ == '__main__':
